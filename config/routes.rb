10fbe1240385879459f284a558f0d789c5e10f33
Nicholas Chee-Awai <cheeordie1@gmail.com>
2017-08-17 06:49:12 -0500

    added all the basic stuff for the website. Needs arrows down the page, but ready for pushing to heroku.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'main#index'
+
+  # Main Pages
+  get "/vision" => "main#vision"
+  get "/initiatives" => "main#initiatives"
+  get "/about" => "main#about"
+  get "/get_involved" => "main#get_involved"
+  get "/news" => "main#news"
 end
